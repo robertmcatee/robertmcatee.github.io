ce788eaaa91d959c525dd18c9288aba83f21b746
Robert MᶜAtee <rtmcatee@gmail.com>
2026-03-17 13:08:00 -0500

    Add files via upload

---
+++ cozycats/sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'catsort-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  './assets/background.png',
+  './assets/house_base.png',
+  './assets/cat_orange.png',
+  './assets/cat_blue.png',
+  './assets/cat_green.png',
+  './assets/cat_purple.png',
+  './assets/Kitten_s_Cozy_Caper.mp3'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
